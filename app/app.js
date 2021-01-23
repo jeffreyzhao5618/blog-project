ebae5517b4dfcfa9e930a88da6bc9a59c3ca7fea
Jeffrey Zhao <jeffreyzhao5618@gmail.com>
2021-01-23 17:43:14 -0600

    updated whitelist

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ const cookieSession = require("cookie-session");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 9000;
-const whitelist = ["http://localhost:3000", "https://cdotgrass.com"];
+const whitelist = ["http://localhost:3000", "https://www.cdotgrass.com"];
 
 const app = express();
 app.use(
