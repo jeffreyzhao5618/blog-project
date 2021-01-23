ea5b2618e1c8a23b823ab3ad58e8a717255ca284
Jeffrey Zhao <jeffreyzhao5618@gmail.com>
2021-01-23 17:53:41 -0600

    updated whitelist

--- app/app.js
+++ app/app.js
@@ -7,8 +7,8 @@ const cors = require("cors");
 const PORT = process.env.PORT || 9000;
 const whitelist = [
   "http://localhost:3000",
-  "https://www.cdotgrass.com",
-  "https://main.d3ij7dq5wbhsdz.amplifyapp.com/",
+  "https://cdotgrass.com",
+  "https://main.d3ij7dq5wbhsdz.amplifyapp.com",
 ];
 
 const app = express();
